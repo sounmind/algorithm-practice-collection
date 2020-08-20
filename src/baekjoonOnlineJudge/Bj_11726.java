224b437b937a4f312dd59c336268a1226ac43f8a
sounmind <sounmind777@gmail.com>
2020-08-20 09:36:02 +0900

    bottom up 버전

--- src/baekjoonOnlineJudge/Bj_11726.java
+++ src/baekjoonOnlineJudge/Bj_11726.java
@@ -3,22 +3,18 @@ package baekjoonOnlineJudge;
 import java.util.Scanner;
 
 public class Bj_11726 { // 2*n 타일링	
-	static int[] dp;
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int n = sc.nextInt();
-		dp = new int[n+1];
-		System.out.println(tile(n));
+		int[] dp = new int[n+1];
+		dp[0] = 1;
+		dp[1] = 1;
+		if(n>=2) {
+			for(int i=2; i<=n; i++) {
+				dp[i] = (dp[i-1] + dp[i-2])%10007;
+			}
+		}
+		System.out.println(dp[n]);
 		sc.close();
-	}
-	public static int tile(int n) {
-		if(n<=2) {
-			return n;
-		}
-		if(dp[n] != 0) {
-			return dp[n];
-		}
-		dp[n] = (tile(n-2)+tile(n-1))%10007;
-		return dp[n];
 	}
 }
