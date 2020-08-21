9ed3cb8d1c8494502bd17d62d8ea3a066375477b
sounmind <sounmind777@gmail.com>
2020-08-21 15:51:23 +0900

    자잘한 수정

--- src/baekjoonOnlineJudge/Bj_9465.java
+++ src/baekjoonOnlineJudge/Bj_9465.java
@@ -1,14 +1,12 @@
 package baekjoonOnlineJudge;
 
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
 
 public class Bj_9465 {
 
-	public static void main(String args[]) throws Exception {
+	public static void main(String args[]) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int Testcase = Integer.parseInt(br.readLine());
         int[][] arr;
