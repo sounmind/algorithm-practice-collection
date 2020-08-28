b26d83af2966175259be527ccb37b26d9808b27c
sounmind <sounmind777@gmail.com>
2020-08-28 15:58:39 +0900

    소수 찾기 제목 추가

--- src/baekjoonOnlineJudge/Bj_1978.java
+++ src/baekjoonOnlineJudge/Bj_1978.java
@@ -4,7 +4,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class Bj_1978 {
+public class Bj_1978 { // 소수 찾기
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
