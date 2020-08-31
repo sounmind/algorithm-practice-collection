bee5eab9693bda4a14d9270b045d62b2636e9460
sounmind <sounmind777@gmail.com>
2020-08-31 17:43:58 +0900

    정수 삼각형 | 통과 | dp배열 점화식 | br
    br을 썼는데도 계속 시간 초과가 나서 의문이었는데, 점화식에서 새로 함수를 선언하는 부분이 시간을 많이 잡아먹었던 것 같다.

--- src/baekjoonOnlineJudge/Bj_1931.java
+++ src/baekjoonOnlineJudge/Bj_1931.java
@@ -1,33 +1,51 @@
 package baekjoonOnlineJudge;
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
-public class Bj_1931 {
+public class Bj_1931 { // 정수 삼각형
+	// 전역 변수 선언
+	static int height; // 높이
+	static int[][] arr; // 삼각형 배열
+	static Integer[][] dp; // 정답(최댓값이 상단꼭짓점에 입력되는) 배열
+	// dp에 객체배열을 쓰면 int배열 보다 4배의 메모리가 소모된다. 하지만 int배열이 0으로 초기화되는 대신 객체배열은 null로 초기화된다.
 	
-	public static int triangle(int[][] arr, int height, int row, int column) {
-		int topValue = arr[row][column];
-		// System.out.printf("topvalue=%d, height=%d, row=%d, column=%d\n", topValue, height, row, column);
-		if(height==1) {
-			return topValue;
-		}
-		int leftMax = triangle(arr, height-1, row+1, column);
-		int rightMax = triangle(arr, height-1, row+1, column+1);
-		return topValue + Math.max(leftMax, rightMax);
-	}
-
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int height = sc.nextInt();
-		
-		int[][] arr = new int[height][height];
-		
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		height = Integer.parseInt(br.readLine());
+		arr = new int[height][height];
+		dp = new Integer[height][height];
+		StringTokenizer st;
+		// 삼각형 값 입력
 		for(int i=0; i<height; i++) {
+			st = new StringTokenizer(br.readLine()," ");
 			for(int j=0; j<=i; j++) {
-				arr[i][j] = sc.nextInt();
-			}
+				arr[i][j] = Integer.parseInt(st.nextToken());
+			}	
 		}
-		System.out.println(triangle(arr, height, 0, 0));
-		sc.close();
+		// 삼각형의 가장 하단 값을 정답 배열로 복사하기
+		for(int i=0; i<height; i++)
+			dp[height-1][i] = arr[height-1][i];
+		
+		System.out.println(getMaxVertex(0, 0));
+	}
+	
+	static int getMaxVertex(int row, int column) {
+		
+		if(row == height - 1)
+			return dp[row][column];
+		
+//		int leftMax = getMaxVertex(row+1, column),
+//			rightMax = getMaxVertex(row+1, column+1); // 이렇게 하면 시간초과
+		
+		if(dp[row][column] == null) // 정답 배열에 값이 없다면,
+//			dp[row][column] = Math.max(leftMax, rightMax) + arr[row][column]; // 이렇게 하면 시간초과
+			dp[row][column] = Math.max(getMaxVertex(row+1, column), getMaxVertex(row+1, column+1)) + arr[row][column];
+		
+		return dp[row][column];
 	}
 
 }
+// 풀이 참고: https://st-lab.tistory.com/131
