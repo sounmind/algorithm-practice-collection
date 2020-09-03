26d2f9cd36e5e57cb5140c9af0cfb69df55e7ca5
sounmind <sounmind777@gmail.com>
2020-09-03 16:38:46 +0900

    9251|LCS|DP|실전을위한끊임없는연습과 깊은이해가 필요해

--- src/baekjoonOnlineJudge/Bj_9251.java
+++ src/baekjoonOnlineJudge/Bj_9251.java
@@ -4,7 +4,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class Bj_9251 { // LCS: Longest Common SubSequence
+public class Bj_9251 {
 
 	public static void main(String[] args) throws IOException { // LCS (Longest Common Subsequence)
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
