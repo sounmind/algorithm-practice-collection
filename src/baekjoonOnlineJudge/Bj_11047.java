bd10a0d0fe7bd1569652cb37a031b2d9c946480a
sounmind <sounmind777@gmail.com>
2020-09-09 17:47:07 +0900

    11047|동전0|그리디|다른사람풀이보고 알고리즘 개선

--- src/baekjoonOnlineJudge/Bj_11047.java
+++ src/baekjoonOnlineJudge/Bj_11047.java
@@ -12,31 +12,20 @@ public class Bj_11047 { // 동전 0 |  K원을 만드는데 필요한 동전 개
 		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 		int N = Integer.parseInt(st.nextToken()); // 동전 단위의 종류
 		int K = Integer.parseInt(st.nextToken()); // 목표 금액
-		
+		int i, coinCount = 0;
 		int[] coins = new int[10];
 		
-		for(int i=0; i<N; i++)
+		for(i=0; i<N; i++)
 			coins[i] = Integer.parseInt(br.readLine());
 		
-		int i;
-		int coinCount = 0;
-		
-		for(i=N-1; i>=0; i--)
-			if(coins[i]<=K)
-				break;
-		
-		while(K >= coins[i]) {
-			K -= coins[i];
-			coinCount++;
-			if(K==0)
-				break;
-			if(K < coins[i]) {
-				while(true)
-					if(K >= coins[--i])
-						break;
+		for(i=N-1; i>=0; i--) {
+			if(coins[i]<=K) {
+				coinCount+=K/coins[i];
+				K=K%coins[i];
 			}
 		}
 		System.out.println(coinCount);
 	}
-
+	
 }
+// 풀이참고: https://www.acmicpc.net/source/15567861
