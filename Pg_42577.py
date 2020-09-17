da66162db8ff2a2740648cebebf9c57fb73ebeed
sounmind <sounmind777@gmail.com>
2020-09-17 12:28:56 +0900

    modified ( little )

--- Pg_42577.py
+++ Pg_42577.py
@@ -15,4 +15,4 @@ def solution(phone_book):
 
 # print(solution(	["119", "97674223", "1195524421"]))
 # print(solution(	["123", "456", "789"]))
-# print(solution(	["12", "123", "1235", "567", "88"]))
+# print(solution(	["12", "123", "1235", "567", "88"])) 
