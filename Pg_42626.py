9944784c753e8bbd53c6f2083256ac4b836418a5
sounmind <sounmind777@gmail.com>
2020-11-03 22:00:43 +0900

    level 2 | 힙 | 더 맵게 | 효율성 테스트까지 통과!

--- Pg_42626.py
+++ Pg_42626.py
@@ -1,16 +1,22 @@
+import heapq
 def solution(scoville, K):
     answer = 0
+    heapq.heapify(scoville)
     while True:
-        scoville.sort(reverse=True)
-        if len(scoville) <= 1 and min(scoville) <= K:
+        try:
+            new_scoville = heapq.heappop(scoville) + heapq.heappop(scoville) * 2
+        except:
             answer = -1
             break
-        scoville.append(scoville.pop() + scoville.pop() * 2)
+        heapq.heappush(scoville, new_scoville)
         answer += 1
-        if min(scoville) >= K:
+        if scoville[0] >= K:
             break
-    print(scoville)
+
     return answer
 
 
-print(solution([1, 2, 3, 9, 10, 12], 100))
+print(solution([1, 2, 3, 9, 10, 12], 7))
+
+
+# 파이썬 힙 모듈 사용법 참고 https://www.daleseo.com/python-heapq/
