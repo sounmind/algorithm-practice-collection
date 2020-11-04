84c56bf0c2dfa24e7b19225d8cbba4e2ce64d597
sounmind <sounmind777@gmail.com>
2020-11-04 23:39:41 +0900

    # 테스트 1, 10, 11실패

--- Pg_42883.py
+++ Pg_42883.py
@@ -2,13 +2,39 @@ from itertools import combinations
 
 
 def solution(number, k):
-    n = len(number)
-    maximum = 0
-    for number_selected in combinations(number, n - k):
-        num = int("".join(number_selected))
-        if maximum <= num:
-            maximum = num
-    return str(maximum)
+    answer = ""
+    number_list = list(map(int, list(number)))
+    answer_len = len(number) - k
+
+    # 첫 값
+    try:
+        max_num = max(number_list[: -k + 1])
+    except:
+        max_num = max(number_list)
+    answer += str(max_num)
+    max_index = -1
+
+    # 두번째 이후 값
+    for i in range(answer_len - 1):
+        max_index = number_list.index(max_num, max_index + 1)
+        try:
+            max_num = max(number_list[max_index + 1 : -answer_len + 2 + i])
+        except:
+            max_num = max(number_list[max_index + 1 :])
+        print("max_index", max_index)
+        answer += str(max_num)
+
+    return answer
 
 
+# 테스트 1, 10, 11실패
+
+print(solution("553", 2))
+print(solution("553", 1))
 print(solution("1924", 2))
+print(solution("1231234", 3))
+print(solution("4177252841", 4))
+print(solution("9999999999", 5))
+print(solution("9991119991", 5))
+
+print([1, 2, 3, 4][1:0])
