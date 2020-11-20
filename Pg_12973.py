65ea0b3cf9002964accac1e37f45a3860eb4284d
sounmind <sounmind777@gmail.com>
2020-11-20 23:39:24 +0900

    짝지어 제거하기 | 두번째 시도 | 시간 초과

--- Pg_12973.py
+++ Pg_12973.py
@@ -1,13 +1,11 @@
 def solution(s):
-    s = list(s)
     while True:
         current_len = len(s)
         if current_len == 0:
             return 1
-        for i, c in enumerate(s):
-            if i > 0 and c == s[i - 1]:
-                s.pop(i)
-                s.pop(i - 1)
+        for i in range(current_len):
+            if i > 0 and s[i - 1] == s[i]:
+                s = s[: i - 1] + s[i + 1 :]
                 break
         if len(s) == current_len:  # 처음 길이와 같다면(제거할 것이 없다면)
             return 0
