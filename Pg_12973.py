094821a0f26c9fdc575a35282c1143b3f31f07cd
sounmind <sounmind777@gmail.com>
2020-11-21 01:17:00 +0900

    짝지어제거하기 | 실패->시간초과

--- Pg_12973.py
+++ Pg_12973.py
@@ -1,15 +1,20 @@
 def solution(s):
+    i = 0
+    origin_len = len(s)
     while True:
-        current_len = len(s)
-        if current_len == 0:
-            return 1
-        for i in range(current_len):
+        try:
             if i > 0 and s[i - 1] == s[i]:
                 s = s[: i - 1] + s[i + 1 :]
-                break
-        if len(s) == current_len:  # 처음 길이와 같다면(제거할 것이 없다면)
+                i = 0
+            else:
+                i += 1
+        except:
+            if len(s) == 0:
+                return 1
+        if i == origin_len:
             return 0
 
 
 print(solution("baabaa"))  # 1
 print(solution("cdcd"))  # 0
+print(solution("fffddcddcddeeffeef"))  # 1
