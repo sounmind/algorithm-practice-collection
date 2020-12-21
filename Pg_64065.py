0aa9c57e5086024af15a188846d905b949a241b0
sounmind <sounmind777@gmail.com>
2020-12-21 17:32:45 +0900

    refactor(level2/튜플): Counter.most_common 사용한 최적해
    - 내가 생각할 때 이것이 가장 간결한 답안

--- Pg_64065.py
+++ Pg_64065.py
@@ -3,17 +3,8 @@ from collections import Counter
 
 
 def solution(s):
-    dict_number_count = Counter(re.findall("\d+", s))
-
-    sorted_item_list = list(
-        sorted(dict_number_count.items(), key=lambda x: x[1], reverse=True)
-    )
-
-    answer = []
-    for key, value in sorted_item_list:
-        answer.append(int(key))
-
-    return answer
+    numbers_sorted_by_frequency = Counter(re.findall("\d+", s)).most_common()
+    return [int(key) for key, value in numbers_sorted_by_frequency]
 
 
 print(solution("{{2},{2,1},{2,1,3},{2,1,3,4}}"))
