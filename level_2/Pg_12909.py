56ed58eef6d60c27fd364e85a7f806f73a12b01d
sounmind <sounmind777@gmail.com>
2020-12-22 13:30:02 +0900

    refactor(python/level2/올바른괄호): 스택 사용!

--- level_2/Pg_12909.py
+++ level_2/Pg_12909.py
@@ -1,9 +1,20 @@
 def solution(s):  # 올바른 괄호
-    for _ in range(len(s)):
-        s = s.replace("()", "")
-    if len(s) > 0:
+    stack = []
+    for element in s:
+        stack.append(element)
+        find_and_delete_pair_of_bracket(stack)
+    if len(stack) > 0:
         return False
     return True
+
+
+def find_and_delete_pair_of_bracket(stack):
+    try:
+        if stack[-2:] == ["(", ")"]:
+            stack.pop()
+            stack.pop()
+    except:
+        pass
 
 
 print(solution("()()"))
