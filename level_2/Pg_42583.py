7b9f2dadaaf1763d52b162eb4465987bca1a6623
sounmind <sounmind777@gmail.com>
2020-12-22 15:50:07 +0900

    refactor(다리를 지나는 트럭): 여전히 통과 못함...

--- level_2/Pg_42583.py
+++ level_2/Pg_42583.py
@@ -4,19 +4,21 @@ from collections import deque
 def solution(bridge_length, weight_bridge_can_hold, truck_weights):
     time = 0
     bridge = deque([0] * bridge_length)
-    truck_weights = deque(truck_weights)
-
-    while bridge:
+    i = 0
+    while True:
+        print(bridge)
         time += 1
         bridge.popleft()
-        if truck_weights:
-            if sum(bridge) + truck_weights[0] <= weight_bridge_can_hold:
-                bridge.append(truck_weights.popleft())
+        if i < len(truck_weights):
+            if sum(bridge) + truck_weights[i] <= weight_bridge_can_hold:
+                bridge.append(truck_weights[i])
+                i += 1
             else:
                 bridge.append(0)
         else:
-            time += len(bridge)
-            return time
+            break
+
+    return time + len(bridge)
 
 
 print(solution(2, 10, [7, 4, 5, 6]))
