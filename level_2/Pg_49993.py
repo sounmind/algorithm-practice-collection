2690028c77349ab080150db6259b62c9910ce4b5
sounmind <sounmind777@gmail.com>
2020-12-23 15:46:23 +0900

    solution(python/스킬트리): 최종 답안

--- level_2/Pg_49993.py
+++ level_2/Pg_49993.py
@@ -1,8 +1,14 @@
+from collections import deque
+
+
 def solution(skill_order, skill_trees):
-
+    answer = len(skill_trees)
+    skill_order = skill_order
+    for skill_tree in skill_trees:
+        skill_order_queue = deque(skill_order)
+        for skill in skill_tree:
+            if skill in skill_order_queue:
+                if skill != skill_order_queue.popleft():
+                    answer -= 1
+                    break
     return answer
-
-
-print(solution("CBD", ["BACDE", "CBADF", "AECB", "BDA"]))
-
-# 선행스킬순서에 있는 스킬을 순서대로 스킬트리에서 찾고 모순이 없는지 확인한다.
