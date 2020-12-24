439411cf2d04bec5cebbbaa1760cb3431193f1d3
sounmind <sounmind777@gmail.com>
2020-12-24 15:58:07 +0900

    solution(괄호 변환): 자력 풀이 성공

--- level_2/Pg_60058.py
+++ level_2/Pg_60058.py
@@ -1,16 +1,55 @@
-# 1. 입력이 빈 문자열인 경우, 빈 문자열을 반환합니다.
-# 2. 문자열 w를 두 "균형잡힌 괄호 문자열" u, v로 분리합니다. 단, u는 "균형잡힌 괄호 문자열"로 더 이상 분리할 수 없어야 하며, v는 빈 문자열이 될 수 있습니다.
-# 3. 문자열 u가 "올바른 괄호 문자열" 이라면 문자열 v에 대해 1단계부터 다시 수행합니다.
-#   3-1. 수행한 결과 문자열을 u에 이어 붙인 후 반환합니다.
-# 4. 문자열 u가 "올바른 괄호 문자열"이 아니라면 아래 과정을 수행합니다.
-#   4-1. 빈 문자열에 첫 번째 문자로 '('를 붙입니다.
-#   4-2. 문자열 v에 대해 1단계부터 재귀적으로 수행한 결과 문자열을 이어 붙입니다.
-#   4-3. ')'를 다시 붙입니다.
-#   4-4. u의 첫 번째와 마지막 문자를 제거하고, 나머지 문자열의 괄호 방향을 뒤집어서 뒤에 붙입니다.
-#   4-5. 생성된 문자열을 반환합니다.
+def solution(given_string):
+    if given_string == "":
+        return ""
+    undividable_balanced, balanced = divide_string_to_2_balanced(given_string)
+    if is_right_bracket(undividable_balanced):
+        return undividable_balanced + solution(balanced)
+    else:
+        return (
+            "(" + solution(balanced) + ")" + reverse_bracket(undividable_balanced[1:-1])
+        )
 
 
-def solution(p):
+def divide_string_to_2_balanced(string):
+    undividable_balanced, balanced = "", ""
+    total_left_bracket, total_right_bracket = 0, 0
+    for i, bracket in enumerate(string):
+        if bracket == "(":
+            total_left_bracket += 1
+        if bracket == ")":
+            total_right_bracket += 1
+        if total_left_bracket == total_right_bracket:
+            undividable_balanced = string[: i + 1]
+            try:  # 오류가 난다면, balanced 가 공백이라는 뜻
+                balanced = string[i + 1 :]
+            except:
+                balanced = ""
+            break
+    return undividable_balanced, balanced
+
+
+def reverse_bracket(string):
     answer = ""
-
+    for i in range(len(string)):
+        if string[i] == "(":
+            answer += ")"
+        else:
+            answer += "("
     return answer
+
+
+def is_right_bracket(string):  # 올바른 괄호
+    stack = []
+    for element in string:
+        stack.append(element)
+        find_and_delete_pair_of_bracket(stack)
+    return len(stack) == 0
+
+
+def find_and_delete_pair_of_bracket(stack):
+    try:
+        if stack[-2:] == ["(", ")"]:
+            stack.pop()
+            stack.pop()
+    except:
+        pass
