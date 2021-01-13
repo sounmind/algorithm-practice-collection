961f3a12a522f5bcc8ce282737b730bee936c579
sounmind <sounmind777@gmail.com>
2021-01-13 18:29:42 +0900

    solution: use two pointers

--- 344_reverse-string.py
+++ 344_reverse-string.py
@@ -1,3 +1,7 @@
 class Solution:
     def reverseString(self, s: List[str]) -> None:
-        s.reverse()
+        left, right = 0, len(s) -1
+        while left < right:
+            s[left], s[right] = s[right], s[left]
+            left += 1
+            right -= 1
