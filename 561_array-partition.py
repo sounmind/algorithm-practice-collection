075b4a30588269fd96084f01c26f9fc8fd88634c
sounmind <sounmind777@gmail.com>
2021-02-02 11:39:49 +0900

    solve(561): sum sorted nums[2n]

--- 561_array-partition.py
+++ 561_array-partition.py
@@ -1,16 +1,12 @@
 def arrayPairSum(nums):
     sum = 0
-    pair = []
     nums.sort()
 
-    for n in nums:
-        pair.append(n)
-        if len(pair) == 2:
-            sum += min(pair)
-            pair = []
+    for i in range(0, len(nums), 2):
+        sum += nums[i]
 
     return sum
 
 
-print(arrayPairSum([1,4,3,2])) # 4
-print(arrayPairSum([6,2,6,5,1,2])) # 9
+print(arrayPairSum([1, 4, 3, 2]))  # 4
+print(arrayPairSum([6, 2, 6, 5, 1, 2]))  # 9
