42b360f1f26902456602b0ffca9ff65c04f1e630
sounmind <sounmind777@gmail.com>
2021-03-09 15:11:06 +0900

    solve(206): iteration

--- 206_reverse-linked-list.py
+++ 206_reverse-linked-list.py
@@ -7,16 +7,16 @@ class ListNode:
 
 class Solution:
     def reverseList(self, head: ListNode) -> ListNode:
-        def reverse(node: ListNode, prev: ListNode = None):
-            if not node:  # node가 None을 가리킬 경우
-                return prev
-            # node가 가리키는 값이 prev로 할당된다.
-            # prev는 node가 첫 노드일 경우 None,
-            # 첫 노드가 아닐 경우 자신을 가리키던 노드가 할당된다.
-            next_node, node.next = node.next, prev
+        if not head:
+            return head
+        prev = head
+        head = head.next
+        prev.next = None
 
-            # node: node가 원래 가리키던 노드
-            # next_node: node가 원래 가리키던 node가 가리키던 노드
-            return reverse(next_node, node)
+        while head != None:
+            next_node = head.next
+            head.next = prev
+            prev = head
+            head = next_node
 
-        return reverse(head)
+        return prev
