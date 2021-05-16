2cfea598d3c0627083620d01661523aa30e7c2e3
suhyeonglee <sounmind777@gmail.com>
2021-05-16 11:35:25 +0900

    solve: lc-66 smart way
    Runtime: 80 ms, faster than 57.46% of JavaScript online submissions for Plus One.
    Memory Usage: 38.8 MB, less than 32.37% of JavaScript online submissions for Plus One.

--- js/lc-66-plus-one.js
+++ js/lc-66-plus-one.js
@@ -2,10 +2,21 @@
  * @param {number[]} digits
  * @return {number[]}
  */
-var plusOne = (digits) =>
-  Array.from((BigInt(digits.join("")) + BigInt(1)).toString()).map(
-    (numString) => parseInt(numString)
-  );
+var plusOne = (digits) => {
+  for (let i = digits.length - 1; i >= 0; i--) {
+    if (digits[i] < 9) {
+      digits[i] += 1;
+
+      return digits;
+    }
+
+    digits[i] = 0;
+  }
+
+  digits.unshift(1);
+
+  return digits;
+};
 
 console.log(plusOne([1, 2, 3]));
 console.log(plusOne([4, 3, 2, 1]));
@@ -14,3 +25,4 @@ console.log(plusOne([9])); // [1, 0]
 console.log(plusOne([9, 9])); // [1, 0, 0]
 console.log(plusOne([6, 1, 4, 5, 3, 9, 0, 1, 9, 5, 1, 8, 6, 7, 0, 5, 5, 4, 3]));
 // [6,1,4,5,3,9,0,1,9,5,1,8,6,7,0,5,5,4,4]
+console.log(plusOne([1, 0, 9])); // [1, 1, 0]
