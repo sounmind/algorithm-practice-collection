aad4b5eeda5198ef82f8fe6e46fecca8b526ba6d
suhyeonglee <sounmind777@gmail.com>
2021-05-26 22:07:15 +0900

    solve: lc-16 faster

--- js/lc-16-3sum-closest.js
+++ js/lc-16-3sum-closest.js
@@ -4,24 +4,29 @@
  * @return {number}
  */
 var threeSumClosest = function (nums, target) {
-  let result = nums[0] + nums[1] + nums[2];
-  let minSub = Math.abs(result - target);
+  let closest = Infinity;
+
+  nums.sort((a, b) => a - b);
 
   for (let i = 0; i < nums.length - 2; i++) {
-    for (let j = i + 1; j < nums.length - 1; j++) {
-      for (let k = j + 1; k < nums.length; k++) {
-        const sum = nums[i] + nums[j] + nums[k];
-        const newSub = Math.abs(sum - target);
+    let [left, right] = [i + 1, nums.length - 1];
 
-        if (newSub < minSub) {
-          minSub = newSub;
-          result = sum;
-        }
+    while (left < right) {
+      const sum = nums[i] + nums[left] + nums[right];
+
+      if (Math.abs(sum - target) < Math.abs(closest - target)) {
+        closest = sum;
+      }
+
+      if (sum > target) {
+        right -= 1;
+      } else {
+        left += 1;
       }
     }
   }
 
-  return result;
+  return closest;
 };
 
 console.log(threeSumClosest([-1, 2, 1, -4], 1));
