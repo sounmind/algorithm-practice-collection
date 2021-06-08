e9bd21e343305f101321b6809019e59b58ebcbed
suhyeonglee <sounmind777@gmail.com>
2021-06-09 00:19:31 +0900

    solve: 프로그래머스 영어 끝말잇기

--- level_2/Pg_12989-english-chainging-words.js
+++ level_2/Pg_12989-english-chainging-words.js
@@ -1,35 +1,65 @@
-// 1번부터 번호 순서대로 한 사람씩 차례대로 단어를 말합니다.
-// 마지막 사람이 단어를 말한 다음에는 다시 1번부터 시작합니다.
-// 앞사람이 말한 단어의 마지막 문자로 시작하는 단어를 말해야 합니다.
-// 이전에 등장했던 단어는 사용할 수 없습니다.
-// 한 글자인 단어는 인정되지 않습니다.
-
 function solution(n, words) {
-  const answer = [];
+  const answers = {};
   let round = 1;
+  let beforeWord = words[0];
+  answers[beforeWord] = true;
 
-  for (let i = 0; i < worlds.length; i++) {
+  for (let i = 1; i < words.length; i++) {
+    let currentWord = words[i];
     if (i % n === 0) {
       round += 1;
     }
+
+    if (beforeWord[beforeWord.length - 1] !== currentWord[0]) {
+      return [(i % n) + 1, round];
+    }
+
+    if (answers[currentWord]) {
+      return [(i % n) + 1, round];
+    } else {
+      answers[currentWord] = true;
+    }
+
+    beforeWord = currentWord;
   }
 
-  return answer; // 가장 먼저 탈락하는 사람의 번호, 그 사람이 자신의 몇 번째 차례에 탈락하는지
+  return [0, 0];
 }
 
-console.log(solution(3, [
-  "tank", "kick", "know", "wheel", "land", 
-  "dream", "mother", "robot", "tank"
-])); // [3, 3]
+console.log(
+  solution(3, [
+    "tank",
+    "kick",
+    "know",
+    "wheel",
+    "land",
+    "dream",
+    "mother",
+    "robot",
+    "tank",
+  ])
+); // [3, 3]
 
-console.log(solution(5, [
-  "hello", "observe", "effect", "take", 
-  "either", "recognize", "encourage", "ensure", 
-  "establish", "hang", "gather", "refer", 
-  "reference", "estimate", "executive"
-])); // [0, 0]
+console.log(
+  solution(5, [
+    "hello",
+    "observe",
+    "effect",
+    "take",
+    "either",
+    "recognize",
+    "encourage",
+    "ensure",
+    "establish",
+    "hang",
+    "gather",
+    "refer",
+    "reference",
+    "estimate",
+    "executive",
+  ])
+); // [0, 0]
 
-console.log(solution(2, [
-  "hello", "one", "even", "never",
-  "now", "world", "draw"
-])) // [1, 3]
+console.log(
+  solution(2, ["hello", "one", "even", "never", "now", "world", "draw"])
+); // [1, 3]
