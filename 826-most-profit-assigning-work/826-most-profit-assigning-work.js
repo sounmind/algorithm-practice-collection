92f0bbc5de48b6b9f25fc5040e77474d16589bb1
Suhyeong Lee (Evan) <37020415+sounmind@users.noreply.github.com>
2022-02-13 15:20:57 +0900

    Time: 1256 ms (24.14%), Space: 45.9 MB (79.31%) - LeetHub

--- 826-most-profit-assigning-work/826-most-profit-assigning-work.js
+++ 826-most-profit-assigning-work/826-most-profit-assigning-work.js
@@ -4,34 +4,20 @@
  * @param {number[]} worker
  * @return {number}
  */
-var maxProfitAssignment = function (difficulties, profits, workerCapabilities) {
-  const profitAndDifficulty = [...profits].map((profit, index) => [
-    profit,
-    difficulties[index],
-  ]);
-  const sortedProfitAndDifficulty = [...profitAndDifficulty].sort(
-    (a, b) => b[0] - a[0]
-  );
-  const sortedWorkerCapabilities = workerCapabilities.sort((a, b) => b - a);
-  const workLength = sortedProfitAndDifficulty.length;
-  const workerLength = workerCapabilities.length;
-
-  let totalProfit = 0;
-  let workIndex = 0;
-  let workerIndex = 0;
-
-  while (workIndex < workLength && workerIndex < workerLength) {
-    const workerCapability = sortedWorkerCapabilities[workerIndex];
-    const work = sortedProfitAndDifficulty[workIndex];
-    const [workProfit, workDifficulty] = work;
-
-    if (workDifficulty > workerCapability) {
-      workIndex += 1;
-    } else {
-      totalProfit += workProfit;
-      workerIndex += 1;
+var maxProfitAssignment = function(difficulty, profit, worker) {
+    let n = difficulty.length;
+    let sum = 0;
+    
+    for (const w of worker) {
+        let max = 0;
+        
+        for (let i = 0; i < n; i++) {
+            if (difficulty[i] > w) continue;
+            
+            max = Math.max(max, profit[i]);
+        }
+        sum += max;
     }
-  }
-
-  return totalProfit;
+    
+    return sum;
 };
