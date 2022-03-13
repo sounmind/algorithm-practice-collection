fcd2de2e33b5dc80aa6def606875cbd6473d6e55
Suhyeong Lee (Evan) <37020415+sounmind@users.noreply.github.com>
2022-03-13 10:45:35 +0900

    Time: 63 ms (85.68%), Space: 42 MB (44.84%) - LeetHub

---
+++ 232-implement-queue-using-stacks/232-implement-queue-using-stacks.js
@@ -0,0 +1,32 @@
+var MyQueue = function() {
+    this.stack = [];
+};
+
+/** 
+ * @param {number} x
+ * @return {void}
+ */
+MyQueue.prototype.push = function(x) {
+    this.stack.push(x);
+};
+
+/**
+ * @return {number}
+ */
+MyQueue.prototype.pop = function() {
+    return this.stack.shift();
+};
+
+/**
+ * @return {number}
+ */
+MyQueue.prototype.peek = function() {
+    return this.stack.at(0);
+};
+
+/**
+ * @return {boolean}
+ */
+MyQueue.prototype.empty = function() {
+    return this.stack.length === 0
+};
